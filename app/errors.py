152b87e5fd01ab99373fc9d3b1e83fb0553dda30
root <root@neo.aeza.network>
2024-11-23 15:53:23 +0000

    add new exception

--- app/errors.py
+++ app/errors.py
@@ -3,3 +3,8 @@
 class OversizeFile(Exception):
     def __init__(self, message: str = "The file size is larger than expected."):
         super().__init__(message)
+
+
+class IsPlaylist(Exception):
+    def __init__(self, message: str = "Playlists are not allowed."):
+        super().__init__(message)
