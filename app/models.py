64bca7fbb356b788f5513f05404700499b15e478
joqd <gorgdis@gmail.com>
2025-09-20 12:44:31 +0000

    feat: add is_admin field to User

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     id = models.BigIntegerField(primary_key=True)
     first_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
