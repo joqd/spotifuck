3a90b85fa8f70600468928a1a2bd5a25572201c9
joqd <gorgdis@gmail.com>
2025-10-04 19:32:25 +0000

    feat: add new promoted_at field to User

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     username = models.CharField(max_length=100, blank=True, null=True)
     is_admin = models.BooleanField(default=False)
     last_request_at = models.DateTimeField(blank=True, null=True)
+    promoted_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
