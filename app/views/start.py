e8c0618103c693f680283ee2bc49959ed06c4c5f
joqd <gorgdis@gmail.com>
2025-10-09 09:55:52 +0000

    pref: better /start response

--- app/views/start.py
+++ app/views/start.py
@@ -3,5 +3,5 @@ from telebot.types import Message
 from adagio import bot
 
 def start_handler(message: Message):
-    bot.reply_to(message, 'Hello World 4')
+    bot.reply_to(message, 'Hello World 5')
     
